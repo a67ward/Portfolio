948b0b4d0b40cd1a5388c3f50396483086794795
a67ward <134404655+a67ward@users.noreply.github.com>
2023-10-18 09:52:04 -0400

    Root Directory Script
    Adds get_root_dir definition to be able to read files from repo

---
+++ get_root_dir.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 18 09:48:27 2023
+
+@author: a67wa_lb2dwvq
+"""
+
+import os
+
+def get_root_dir():
+    return os.path.dirname(os.path.abspath(__file__))
